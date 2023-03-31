f23cd63ab16c5ec193d303058953f9bb86d97bbb
brightchul <abhbtbb1@gmail.com>
2023-03-31 13:27:38 +0900

    feat: add CircleArea component and useCircleArea hook

---
+++ collision1/src/components/CircleArea/CircleArea.tsx
@@ -0,0 +1,30 @@
+import { memo, useEffect, useRef, useState } from "react";
+import { CircleManager } from "../../circle";
+import useCircleArea from "./useCircleArea";
+
+export interface CircleAreaProps {
+  id: string;
+  height: number;
+  width: number;
+  posX?: number;
+  posY?: number;
+  bgColor: string;
+  circleManager: CircleManager;
+  isPlay?: boolean;
+}
+
+function CircleArea(props: CircleAreaProps) {
+  const { canvasRef, toggleAnimation } = useCircleArea(props);
+
+  useEffect(() => {
+    toggleAnimation();
+  }, [props.isPlay]);
+
+  return (
+    <div id={props.id}>
+      <canvas ref={canvasRef}></canvas>
+    </div>
+  );
+}
+
+export default memo(CircleArea);
