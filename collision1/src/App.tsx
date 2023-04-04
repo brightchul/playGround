b06d67f616d609acca5d8d68912329d5b57f9ce9
brightchul <abhbtbb1@gmail.com>
2023-04-04 20:13:51 +0900

    chore: setting tailwindcss

--- collision1/src/App.tsx
+++ collision1/src/App.tsx
@@ -3,6 +3,7 @@ import CircleCollisionPage from "./pages/CircleCollision";
 function App() {
   return (
     <div>
+      <h1 className="text-3xl font-bold underline">Hello world!111111111111</h1>
       <CircleCollisionPage />
     </div>
   );
