929f525892423d748155828f78d34982295e6df9
brightchul <abhbtbb1@gmail.com>
2023-04-08 20:53:41 +0900

    feat: add fold button

--- collision1/src/components/SettingCard/index.tsx
+++ collision1/src/components/SettingCard/index.tsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 
+import { useToggle } from "../../hooks";
 import {
   selectConfigsById,
   useCircleConfigStore,
 } from "../../stores/circleConfig";
 import { configNamesEntries } from "./constants";
+import FoldButton from "./foldButton";
 import SettingInputNumber from "./SettingInput";
 import { ConfigNamesKeysType } from "./types";
 import { useSettingCardValidation } from "./validations";
@@ -52,39 +54,46 @@ export default function SettingCard({
     handleSubmit(onSubmit)();
   }, []);
 
+  const [isFold, toggleFold] = useToggle(false);
+
   return (
     <div
       className={`w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 ${wrapperClassName}`}
     >
-      <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
-        {configNamesEntries.map(([name, label]) => (
-          <SettingInputNumber
-            key={`${name}-${label}`}
-            name={name}
-            label={label}
-            control={control}
-            defaultValue={config[name]}
-            rules={validationRules[name]}
-          />
-        ))}
+      <div className="absolute top-1 right-1">
+        <FoldButton handleClick={toggleFold} />
+      </div>
+      {!isFold && (
+        <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
+          {configNamesEntries.map(([name, label]) => (
+            <SettingInputNumber
+              key={`${name}-${label}`}
+              name={name}
+              label={label}
+              control={control}
+              defaultValue={config[name]}
+              rules={validationRules[name]}
+            />
+          ))}
 
-        <div className="flex gap-3">
-          <button
-            className="mt-5 w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-            disabled={!formState.isValid}
-          >
-            {isPlay === false ? "play" : "stop"}
-          </button>
-          <button
-            type="button"
-            onClick={() => removeConfig(id)}
-            className="mt-5 w-full text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-            disabled={!formState.isValid}
-          >
-            remove
-          </button>
-        </div>
-      </form>
+          <div className="flex gap-3">
+            <button
+              className="mt-5 w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              disabled={!formState.isValid}
+            >
+              {isPlay === false ? "play" : "stop"}
+            </button>
+            <button
+              type="button"
+              onClick={() => removeConfig(id)}
+              className="mt-5 w-full text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              disabled={!formState.isValid}
+            >
+              remove
+            </button>
+          </div>
+        </form>
+      )}
     </div>
   );
 }
